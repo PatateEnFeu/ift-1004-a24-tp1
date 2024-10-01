a4158b008a6a13437837b107fefba2103ffa7681
PotatoFireee <ibrahim.traore.jr@outlook.fr>
2024-10-01 11:53:20 -0400

    correction exo 1 et fin tp1

--- exercice1.py
+++ exercice1.py
@@ -15,10 +15,10 @@ print("\nLa meilleure compagnie aérienne du monde !")
 nbr_passagers = int(input('\nCombien de billets souhaitez-vous réserver: '))
 num_passager = 1
 
-prix_billet1 = 0.00
-prix_billet2 = 0.00
-prix_billet3 = 0.00
-prix_global = 0.00
+prix_billet1 = 0
+prix_billet2 = 0
+prix_billet3 = 0
+prix_global = 0
 
 choix = 0
 
@@ -37,9 +37,9 @@ while choix != 2:
         rabais = 0
         if age <= 3 :
             rabais = 0
-            prix_billet = 0.00
+            prix_billet = 0
             prix_billet1 += prix_billet
-            print(f'\nPrix du billet réservé pour {nom}: {prix_billet}$')
+            print(f'\nPrix du billet réservé pour {nom}: {prix_billet:.2f}$')
             num_passager += 1
 
         elif age >= 65 :
@@ -47,23 +47,23 @@ while choix != 2:
             prix_billet = 150 - (150 * (rabais/100))
             prix_billet2 += prix_billet
             print(f'Rabais appliqué pour {nom}: {rabais}%')
-            print(f'\nPrix du billet réservé pour {nom}: {prix_billet}$')
+            print(f'\nPrix du billet réservé pour {nom}: {prix_billet:.2f}$')
             num_passager += 1
         else:
-            prix_billet = 150.00
+            prix_billet = 150
             prix_billet3 += prix_billet
-            print(f'\nPrix du billet réservé pour {nom}: {prix_billet}$')
+            print(f'\nPrix du billet réservé pour {nom}: {prix_billet:.2f}$')
             num_passager += 1
     
     if nbr_passagers != 0 :
         prix_global += prix_billet1 + prix_billet2 + prix_billet3 #Valeur à ne pas réinitialiser pour le prix cumulé
         prix_total = prix_billet1 + prix_billet2 + prix_billet3 #Valeurs qui seront réinitialiser à la fin de la boucle
-        print (f"\nLe montant total à encaisser cette réservation est: {prix_total}$")
+        print (f"\nLe montant total à encaisser pour cette réservation est: {prix_total:.2f}$")
 
-        print(f'\n1. Enregistrer un autre client')
-        print(f'2. Quitter')
-        print(f'Entrez votre choix: ', end="")
-        choix = int(input(''))
+    print(f'\n1. Enregistrer un autre client')
+    print(f'2. Quitter')
+    print(f'Entrez votre choix: ', end="")
+    choix = int(input(''))
 
     while choix != 1 and choix != 2 :
         print(f'Choix invalide, veuillez entrer 1 ou 2: ',end="")
@@ -80,5 +80,5 @@ while choix != 2:
         choix = 0
 #Fin code quand l'utilisateur fait le choix 2 en affichant le montant total cumulè
     if choix == 2 :
-        print(f'\nMontant total cumulé pour tous les clients enregistrés : {prix_global}$')
+        print(f'\nMontant total cumulé pour tous les clients enregistrés : {prix_global:.2f}$')
         print(f'Bye bye!')
