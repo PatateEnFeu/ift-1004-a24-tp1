e94e65dc28f10e9dca4eaec73dd8b1bd7a77db89
PotatoFireee <ibrahim.traore.jr@outlook.fr>
2024-10-04 16:50:08 -0400

    correction exercice 2 et fin tp1

--- exercice2.py
+++ exercice2.py
@@ -1,10 +1,8 @@
 hauteur = int(input(f"Entrez la hauteur: "))
-lignes = hauteur + 1
-
 if hauteur < 4 :
    print(f"Hauteur incorrecte")
 
-elif hauteur > 4 :
+elif hauteur >= 4 :
     for i in range(1, hauteur+1):
         print(' ' * (hauteur - i), end='')
         for j in range(1, i + 1):
